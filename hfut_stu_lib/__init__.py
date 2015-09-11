54e827721799b4f678b7e1dfd7cd6cba528f4a61
erliang <eviler_liang@foxmail.com>
2015-09-11 20:36:48 +0800

    - 20150911 v0.1.0     1. 解决 requests 不能对 GBK 转 UTF8 无损转换的问题     2. 添加 StuLib.catch_response , 抽象了响应的获取, 提升了代码的可维护性

--- hfut_stu_lib/__init__.py
+++ hfut_stu_lib/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 __title__ = 'hfut_stu_lib'
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 __author__ = 'erliang'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 erliang'
